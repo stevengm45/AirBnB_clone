3a93786f1ab34456f3a657d05148b159613ff0a2
usuario <usuario@usuario-pc>
2021-06-29 14:56:05 -0500

    fix style pep8 and new files, state and review

--- models/user.py
+++ models/user.py
@@ -4,9 +4,10 @@ This is class User
 """
 from models.base_model import BaseModel
 
+
 class User(BaseModel):
-  """class User display the information of user"""
-  email = ""
-  password = ""
-  first_name = ""
-  last_name = ""
+    """class User display the information of user"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
