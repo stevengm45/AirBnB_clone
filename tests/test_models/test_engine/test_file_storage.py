8c6914f2e103199b42d3d4b0e5cb1bc376eebad8
Diana Henao <2777@holbertonschool.com>
2021-06-29 23:11:45 +0000

    Add unittest to method all of FileStorage

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+   FileStorage tests
+"""
+
+
+class TestFileStorage_all(unittest.TestCase):
+    """
+       Unittest for all method - class FileStorage
+    """
+    pass
