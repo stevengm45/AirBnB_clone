59486d84121b3375aa55bce011119fcda58246af
Diana Henao <2777@holbertonschool.com>
2021-06-29 22:00:11 +0000

    Add number_bathrooms attribute and change to numbers some strings

--- models/place.py
+++ models/place.py
@@ -13,9 +13,10 @@ class Place(BaseModel):
     user_id = ""
     name = ""
     description = ""
-    number_rooms = ""
-    max_guest = ""
-    price_by_night = ""
-    latitude = ""
-    longitude = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
     amenity_ids = ""
