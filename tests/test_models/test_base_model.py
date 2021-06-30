7f48f2dc237546aa3ec3a6652e83e7a8be8bfbed
Diana Henao <2777@holbertonschool.com>
2021-06-30 16:45:59 +0000

    Update test_base_model.py with new tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,10 +2,49 @@
 """
    BaseModel tests
 """
+from models.base_model import BaseModel
+import models
+import unittest
 
 
-class TestBaseModel_save(unittest.TestCase):
+class TestBaseModel(unittest.TestCase):
     """
-       Unittest for save method - class BaseModel
+       Unittest for class BaseModel
     """
-    pass
+    def test_id(self):
+        """Test to prove that ids are different"""
+        id_a = BaseModel()
+        id_b = BaseModel()
+        self.assertNotEqual(id_a, id_b)
+
+    def test__str__(self):
+        """Test to prove that the return is a string"""
+        instance = BaseModel()
+        self.assertTrue(instance.__str__(), type(str))
+
+    def test_to_save(self):
+        """Test to prove if the created and updated time is different"""
+        instance = BaseModel()
+        old_date_created = instance.created_at
+        old_date_updated = instance.updated_at
+        instance.save()
+        new_date_created = instance.created_at
+        new_date_updated = instance.updated_at
+        self.assertEqual(old_date_created, new_date_created)
+        self.assertNotEqual(old_date_updated, new_date_updated)
+
+    def test_to_dict(self):
+        """Test to prove if the return is type dict and the
+           values are different
+        """
+        model_a = BaseModel()
+        model_b = BaseModel()
+        model_dict_a = model_a.to_dict()
+        model_dict_b = model_b.to_dict()
+        self.assertNotEqual(model_a.created_at, model_b.created_at)
+        self.assertNotEqual(model_a.updated_at, model_b.updated_at)
+        self.assertTrue(model_dict_a, type(dict))
+        self.assertTrue(model_dict_b, type(dict))
+
+if __name__ == '__main__':
+    unittest.main()
