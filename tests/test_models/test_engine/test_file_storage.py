43e72d057fc51c4876c77cb9197b2c7ef73cda88
Diana Henao <2777@holbertonschool.com>
2021-07-01 01:46:02 +0000

    Upload test file test_file_storage.py

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -2,10 +2,25 @@
 """
    FileStorage tests
 """
+import models
+import unittest
 
 
-class TestFileStorage_all(unittest.TestCase):
+class TestFileStorage(unittest.TestCase):
     """
-       Unittest for all method - class FileStorage
+       Unittest for class FileStorage - class FileStorage
     """
-    pass
+    def test_instance(self):
+        """Test to prove the instantiation of an
+        object class Amenity
+        """
+        instance = models.FileStorage()
+        self.assertTrue(models.FileStorage, type(instance))
+
+    def test_all(self):
+        """Test the method all to print dict"""
+        obj = models.storage.all()
+        self.assertTrue(dict, type(obj))
+
+if __name__ == '__main__':
+    unittest.main()
